96db4bed529abd1635d79e126eb6459d35d893bc
Amir Khabbaz <amir.khabbaz@gmail.com>
2018-12-15 16:05:31 +0330

    try to resolve the merge conflict

---
+++ Amirhossein Khabbaz/Migrations/201812151200535_Update_Domain_Model.cs
@@ -0,0 +1,67 @@
+namespace Amirhossein_Khabbaz.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Update_Domain_Model : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Educations",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Degree = c.String(nullable: false, maxLength: 255),
+                        University = c.String(nullable: false, maxLength: 255),
+                        PersonId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Interests",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 255),
+                        PersonId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Languages",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 255),
+                        MaximumValue = c.Byte(nullable: false),
+                        CurrentValue = c.Byte(nullable: false),
+                        PersonId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.WorkExperiences",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Company = c.String(nullable: false, maxLength: 255),
+                        Years = c.Byte(nullable: false),
+                        ReasonOfDeparture = c.String(nullable: false, maxLength: 255),
+                        PersonId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            AlterColumn("dbo.Skills", "Name", c => c.String(nullable: false, maxLength: 255));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Skills", "Name", c => c.String(nullable: false));
+            DropTable("dbo.WorkExperiences");
+            DropTable("dbo.Languages");
+            DropTable("dbo.Interests");
+            DropTable("dbo.Educations");
+        }
+    }
+}
